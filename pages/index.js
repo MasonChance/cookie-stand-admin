6a5915f4bb8864a03ef3db2a639afb8df4898493
MasonChance <mason.fryberger@gmail.com>
2021-04-10 20:45:06 -0700

    outline updated, dev server working

--- pages/index.js
+++ pages/index.js
@@ -1,81 +1,52 @@
 import Head from 'next/head'
+import { stringify } from 'postcss'
 
 export default function Home() {
+  const layoutGuide = (tailwind, dev=true)=> {
+    if (dev === true){
+      return ('border-2 border-dotted border-red-500' + tailwind)  
+    } else {
+      return tailwind
+    }
+  }
+
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen py-2">
+    <div className={layoutGuide('')}>
       <Head>
-        <title>Create Next App</title>
+        <title>Cookie Stand Admin</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className="flex flex-col items-center justify-center flex-1 px-20 text-center">
-        <h1 className="text-6xl font-bold">
-          Welcome to{' '}
-          <a className="text-blue-600" href="https://nextjs.org">
-            Next.js!
-          </a>
-        </h1>
+      <header className="pl-2.5 px-5 mx-auto bg-green-300">
+        <h1 className="font-semibold text-4x1 ">Cookie Stand Admin</h1>
+      </header>
 
-        <p className="mt-3 text-2xl">
-          Get started by editing{' '}
-          <code className="p-3 font-mono text-lg bg-gray-100 rounded-md">
-            pages/index.js
-          </code>
+
+      <main className={layoutGuide("w-auto mx-20 mt-2 ",false)}>
+
+        <form className={layoutGuide("grid grid-cols-7 grid-rows-6 col-gap-4 bg-green-300 rounded ")} name='standInfo'>
+          <legend className={layoutGuide('row-start-1 row-span-1 col-start-1 col-span-7 text-center',false)}>Create Cookie Stand</legend>
+          
+          <label className={layoutGuide('row-start-2 row-span-1 col-start-1 col-end-2 '),false} for="location">Location:</label>
+          <input className={layoutGuide( 'row-start-2 row-span-1 col-start-2 col-end-8 '),false} type="text" name="location"></input>
+            
+
+          
+        </form>
+
+        <p className="pl-100">
+          table coming soon!
         </p>
 
-        <div className="flex flex-wrap items-center justify-around max-w-4xl mt-6 sm:w-full">
-          <a
-            href="https://nextjs.org/docs"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Documentation &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Find in-depth information about Next.js features and API.
-            </p>
-          </a>
+        <p className="">
+          {/* json output of last entered cookie stand data */}
+        </p>
 
-          <a
-            href="https://nextjs.org/learn"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Learn &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Learn about Next.js in an interactive course with quizzes!
-            </p>
-          </a>
 
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Examples &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Discover and deploy boilerplate example Next.js projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Deploy &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
       </main>
 
-      <footer className="flex items-center justify-center w-full h-24 border-t">
-        <a
-          className="flex items-center justify-center"
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className="h-4 ml-2" />
-        </a>
+      <footer className="">
+       {/* copywrite 2021 */}
       </footer>
     </div>
   )
