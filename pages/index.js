ad12e92d45dd989ecb04cd2f980c259e38fc3d6b
MasonChance <mason.fryberger@gmail.com>
2021-04-14 12:38:39 -0700

    features for React I lab complete

--- pages/index.js
+++ pages/index.js
@@ -9,7 +9,7 @@ export default function Home() {
       return tailwind
     }
   }
-
+  
   return (
     <div className={layoutGuide('')}>
       <Head>
@@ -23,24 +23,22 @@ export default function Home() {
 
 
       <main className={layoutGuide("mx-20 mt-2 ",false)}>
-        <form className={layoutGuide('w-7/8 grid grid-rows-4 grid-cols-4 gap-1')}>
+        <form className={layoutGuide('w-7/8 h-7/8 grid grid-rows-4 grid-cols-4 items-baseline px-3 bg-green-300')}>
           <legend className={layoutGuide('row-span-1 col-span-4 self-center text-center')}>Create Cookie Stand</legend>
 
-          <label for='location' className={layoutGuide('row-start-2 col-start-1 ml-2  ')}>Location:</label> 
-          <input name='location' type='text' className={layoutGuide('row-start-2 col-start-2 col-span-2 self-end w-5/6 pr-3 mx-5')}></input>
+          <label htmlFor='location' className={layoutGuide('row-start-2 col-start-1 ml-2')}>Location:</label> 
+          <input name='location' type='text' className={layoutGuide('row-start-2 col-start-2 col-span-3 w-7/8 pr-3 mx-5 bg-white')}></input>
 
-          <label for='minCust' className={layoutGuide('row-start-3 col-start-1')}>Minimum Customers per Hour</label>
-          <input name='minCust' type='text' className={layoutGuide(' row-start-4 col-start-1')}></input>
+          <label htmlFor='minCust' className={layoutGuide('row-start-3 col-start-1')}>Minimum Customers per Hour</label>
+          <input name='minCust' type='text' className={layoutGuide(' row-start-4 col-start-1 bg-white')}></input>
           
-
-          <label for='maxCust' className={layoutGuide('row-start-3 col-start-2 ')}>Maximum Customers per Hour</label>
-          <input name='maxCust' type='text' className={layoutGuide('row-start-4 col-start-2')}></input>
+          <label htmlFor='maxCust' className={layoutGuide('row-start-3 row-end-4 col-start-2')}>Maximum Customers per Hour</label>
+          <input name='maxCust' type='text' className={layoutGuide('row-start-4 col-start-2 bg-white')}></input>
           
-
-          <label for='avgCookie' className={layoutGuide('row-start-3 col-start-3 ')}>Average Cookies per Sale</label>
-          <input name='avgCookie' type='text' className={layoutGuide('row-start-4 col-start-3')}></input>
+          <label htmlFor='avgCookie' className={layoutGuide('row-start-3 row-end-4 col-start-3')}>Average Cookies per  <br/>Sale </label>
+          <input name='avgCookie' type='text' className={layoutGuide('row-start-4 col-start-3 bg-white')}></input>
           
-          <button type='submit' className={layoutGuide('row-start-3 col-start-4')}>Create</button>
+          <button type='submit' className={layoutGuide('row-start-3 row-end-4 col-start-4 col-end-5 self-end h-5/6 bg-green-600')}>Create</button>
           
 
         </form>
@@ -52,7 +50,7 @@ export default function Home() {
         </p>
 
         <p className="">
-          {/* json output of last entered cookie stand data */}
+          {String.fromCharCode(123) + "'location' " + String.fromCharCode(58) + " 'Barcelona', 'minCustomers' " + String.fromCharCode(58) + ' 2, ' + "'maxCustomers' : 4, " + "'avgCookies' " + String.fromCharCode(58) + ' 2.5 ' + String.fromCharCode(125)}
         </p>
 
 
